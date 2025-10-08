799be4be7505241033fc73d38d25094b7e40273a
sergiomenen <132551841+sergiomenen@users.noreply.github.com>
2025-10-08 17:30:48 +0200

    Delete hash_utils.py

--- hash_utils.py
+++
@@ -1,4 +0,0 @@
-import hashlib, hmac, os, binascii
-DEFAULT_ALGO='sha256'; SUPPORTED=['sha256','sha1','sha512','blake2b']
-def _get_hasher(a): return hashlib.new(a)
-def hash_text(t,a=DEFAULT_ALGO): h=_get_hasher(a);h.update(t.encode());return h.hexdigest()
